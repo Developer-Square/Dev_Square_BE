14f8644f84081f4e4e112c8bde0616fdf129fee9
saisilinus <saisilinus19@gmail.com>
2020-11-05 02:42:12 -0500

    updated roles to accomodate tasks + portfolio

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,8 +1,8 @@
-const roles = ['user', 'admin', 'developer'];
+const roles = ['user', 'admin'];
 
 const roleRights = new Map();
 roleRights.set(roles[0], []);
-roleRights.set(roles[1], ['getUsers', 'manageUsers']);
+roleRights.set(roles[1], ['getUsers', 'manageUsers', 'getTasks', 'manageTasks', 'getPortfolio', 'managePortfolio']);
 
 module.exports = {
   roles,
