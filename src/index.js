8a31e233f2598ca907cf11bbc259b1377d02c94b
danielkingori <kingndiritu@gmail.com>
2020-11-29 11:35:57 +0300

    heroku changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
 const { userService } = require('./services');
+require('dotenv').config();
 
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(async () => {
   logger.info('Connected to MongoDB');
-  server = app.listen(config.port || 3000, () => {
+  server = app.listen(process.env.PORT || 3000, () => {
     logger.info(`Listening to port ${config.port}`);
   });
 
