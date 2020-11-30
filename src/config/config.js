0157f38ca598a1eaa49fc72b71c575eda26f44f5
danielkingori <kingndiritu@gmail.com>
2020-11-30 09:59:02 +0300

    fix: added atlas uri

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,7 @@ const envVarsSchema = Joi.object()
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
+    MONGODB_ATLAS_URL: Joi.string().required().description('Mongo DB Atlas url'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
@@ -30,11 +31,26 @@ if (error) {
   throw new Error(`Config validation error: ${error.message}`);
 }
 
+const changeMongoUrl = () => {
+  switch (envVars.NODE_ENV) {
+    case 'test':
+      // eslint-disable-next-line prefer-template
+      return envVars.MONGODB_URL + '-test';
+    case 'production':
+      return envVars.MONGODB_ATLAS_URL;
+    case 'development':
+      return envVars.MONGODB_URL;
+    default:
+      return envVars.MONGODB_URL;
+  }
+};
+
 module.exports = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
   mongoose: {
-    url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+    // url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+    url: changeMongoUrl(),
     options: {
       useCreateIndex: true,
       useNewUrlParser: true,
