03cf4f95841acae6a4bc051db171a6ba2347fade
danielkingori <kingndiritu@gmail.com>
2020-11-30 10:01:12 +0300

    Fix: commit issues

--- src/controllers/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.authController = require('./auth.controller');
-module.exports.userController = require('./user.controller');
-module.exports.portfolioController = require('./portfolio.controller');
-module.exports.taskController = require('./task.controller');
--- src/models/index.js
+++
@@ -1,6 +0,0 @@
-const { Task } = require('./task.model');
-
-module.exports.Token = require('./token.model');
-module.exports.User = require('./user.model');
-module.exports.Task = Task
-module.exports.Portfolio = require('./portfolio.model');
