291947c0b96b8e1be1ea83041e23308496858aef
saisilinus <kingndiritu@gmail.com>
2020-11-30 14:09:15 +0300

    Feature: added client services

--- src/services/index.js
+++ src/services/index.js
@@ -4,3 +4,4 @@ module.exports.tokenService = require('./token.service');
 module.exports.userService = require('./user.service');
 module.exports.portfolioService = require('./portfolio.service');
 module.exports.taskService = require('./task.service');
+module.exports.clientService = require('./client.service');
