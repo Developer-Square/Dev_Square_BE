3e4150cb2c53024ec04ff4a1d786a9b03eec0ee9
saisilinus <kingndiritu@gmail.com>
2020-11-30 11:21:04 +0300

    Fix: swagger url

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -1,5 +1,5 @@
 const { version } = require('../../package.json');
-const config = require('../config/config');
+// const config = require('../config/config');
 
 const swaggerDef = {
   openapi: '3.0.0',
@@ -13,7 +13,7 @@ const swaggerDef = {
   },
   servers: [
     {
-      url: `https://developer-square-be.herokuapp.com:${config.port}/v1`,
+      url: `https://developer-square-be.herokuapp.com/v1`,
     },
   ],
 };
